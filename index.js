724a3b42dd48b99cb41ed4afab63a9a99fc0433a
rk28284 <112754760+rk28284@users.noreply.github.com>
2022-11-13 18:33:32 +0530

    day-5

--- index.js
+++ index.js
@@ -4,4 +4,14 @@ document.querySelector("#womenclick").addEventListener("click",function(){
 document.querySelector("#signupclick").addEventListener("click",function(){
     window.location.href="signup.html";
  })
- 
+ document.querySelector("#arrivalclick").addEventListener("click",function(){
+   window.location.href="arrival.html";
+})
+
+document.querySelector("#containerclick").addEventListener("click",function(){
+   window.location.href="arrival.html";
+})
+document.querySelector(".button").addEventListener("click",function(){
+   window.location.href="arrival.html";
+})
+
